d424bd2cd6b6fb0b718e6bcc1293c3bb2c5e1a84
Shalini <chauhanshaliniuk@gmail.com>
2022-04-20 22:04:30 +0530

    removing warnings

--- src/routes/Dashboard/SidePopUp/index.js
+++ src/routes/Dashboard/SidePopUp/index.js
@@ -1,23 +1,14 @@
 import React, { useState } from "react";
-import { connect } from "react-redux";
-import Button from "../../../components/sharedcomponents/Button";
-import { handleFolderApi } from "../../../store/actions";
+
 import "../popUpstyles.css";
-import Input from "../../../components/sharedcomponents/Input";
-import PopUp from "../PopUp";
+
 import SubFolderPopUp from "../SubFolderPopUp";
 const SidePopUp = (props) => {
-  const handleClick = () => {
-    props.toggle();
-  };
   const [subFoldermodal, setSubFoldermodal] = useState(false);
   const addSubPopUp = () => {
     setSubFoldermodal(!subFoldermodal);
   };
-  const handleFolderNameChange = (e) => {
-    setFoldername(e.target.value);
-  };
-  const [foldername, setFoldername] = useState("");
+
   return (
     <div className="sidemodal">
       <ul onClick={addSubPopUp}>
