aa3ef2b259efd5a73216fdabcec55d1fd0bd78b4
Shalini <chauhanshaliniuk@gmail.com>
2022-04-22 19:49:17 +0530

    added regex

--- src/routes/Signin/index.js
+++ src/routes/Signin/index.js
@@ -1,5 +1,5 @@
 import "../../components/sharedcomponents/styles.css";
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { loginData } from "../../store/actions";
